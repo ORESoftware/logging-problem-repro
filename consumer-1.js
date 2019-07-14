9d29677cde96d1b516f47d0876590f82379cf8f4
Operations Research Engineering Software+ <alex@oresoftware.com>
2019-07-13 17:24:08 -0700

    mone

--- consumer-1.js
+++ consumer-1.js
@@ -4,8 +4,14 @@ const path = require('path');
 
 const logfile = path.resolve(process.cwd() + '/temp.log');
 
-const stdinStream = process.stdin.resume()
-  .pipe(fs.createWriteStream(logfile));
+let stdinStream = null;
+
+const createStdinStream = () => {
+  stdinStream = process.stdin.resume()
+    .pipe(fs.createWriteStream(logfile));
+};
+
+createStdinStream();
 
 const container = {
   capAmount: 2000  // 10K bytes
@@ -17,11 +23,17 @@ const handleFileExcess = () => {
   const r = fs.readFileSync(logfile, {encoding:'utf8'});
   
   if(r.length > container.capAmount){
+    
+   
+    stdinStream.close();
+    
     const b = r.slice(r.length - container.capAmount); // take the last 10K bytes
     console.log();
     console.log('b len:', b.length);
     console.log('r len:', r.length);
     fs.writeFileSync(logfile, b, {encoding:'utf8'});
+    
+    createStdinStream();
   }
   
 };
